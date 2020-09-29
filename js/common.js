59102b60d43f0509bf925c25d998d0265065681f
Максим Казачков <evgenshtanko@ya.ru>
2020-09-29 14:11:40 +0400

    'prise'

--- js/common.js
+++ js/common.js
@@ -19,30 +19,41 @@ $('.reviews').slick({
 });
 
 $('.prise').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
   centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
+  variableWidth: true
 });
+
+// $('.prise').slick({
+//   centerMode: true,
+//   centerPadding: '60px',
+//   slidesToShow: 3,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 3
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 1
+//       }
+//     }
+//   ]
+// });
+
+
 
 inpTel.addEventListener('focus', _ => {
   if(!/^\+\d*$/.test(inpTel.value))
