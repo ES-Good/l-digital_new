69f93f058866176df2d295d166a8bb5429235378
ES-Good <46601455+ES-Good@users.noreply.github.com>
2020-10-23 10:16:03 +0400

    'test'

--- js/common.js
+++ js/common.js
@@ -1,4 +1,10 @@
 let inpTel = document.getElementById('tel');
+let negativeBtn = document.getElementById('negative-btn');
+let positiveBtn = document.getElementById('positive-btn');
+
+let negativeContainer = document.querySelector('.negative');
+let positiveContainer = document.querySelector('.positive');
+
 
 $('.mobile-ofers').slick({
   dots: true,
@@ -36,3 +42,19 @@ inpTel.addEventListener('keypress', e => {
   if(!/\d/.test(e.key))
     e.preventDefault();
 });
+
+negativeBtn.onclick = function () {
+  positiveBtn.classList.remove('active-border-b-red')
+  negativeBtn.classList.add('active-border-b-red');
+
+  negativeContainer.classList.add('active-emotion');
+  positiveContainer.classList.remove('active-emotion');
+}
+
+positiveBtn.onclick = function () {
+  negativeBtn.classList.remove('active-border-b-red')
+  positiveBtn.classList.add('active-border-b-red');
+
+  negativeContainer.classList.remove('active-emotion');
+  positiveContainer.classList.add('active-emotion');
+}
